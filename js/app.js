630ddc023e4fe3f65685b9cd0646351ed1700ce3
PauLeeR <lindaliel@gmail.com>
2017-06-01 14:31:02 -0400

    modal no funciona

--- js/app.js
+++ js/app.js
@@ -2,28 +2,36 @@
  * Archivo principal de funcionalidad de JS
  */
  (function main(){
- 	var boxes = Array.from(document.getElementsByClassName("box-services"));
- 	var modal = document.getElementById("box-services-modal");
- 	var bodyModal, close, img;
+ 	var boxes = Array.from(document.getElementsByClassName("col-4"));
+ 	var work = document.getElementById("work");
+ 	
+ 	var modal, bodyModal, close, imagenC;
  	boxes.forEach(function(box){
  		box.addEventListener("click", function(){
- 		modal.innerHTML = "";
-		bodyModal = document.createElement("div");
- 		bodyModal.classList.add("modal-body");
-		bodyModal.innerHTML = box.innerHTML;
-		modal.appendChild(bodyModal);
- 		modal.classList.remove("hide");
- 		close = document.createElement("div");
- 		close.classList.add("close");
- 		img = document.createElement("img");
- 		img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
- 		close.appendChild(img);
- 		modal.appendChild(close);
- 		close.addEventListener("click",function(){
-				modal.classList.add("hide");
-			});
+ 			//modal.innerHTML = "";
+ 			modal = document.createElement("div");
+ 			modal.classList.add("modal-work");
+ 			bodyModal = document.createElement("div");
+ 			bodyModal.classList.add("modal-body");
+ 			bodyModal.innerHTML = box.innerHTML;
+ 			modal.appendChild(bodyModal);  
+ 			//creando eleementos de manera dinàmica
+ 			close = document.createElement("div");
+ 			close.classList.add("close");
+ 			//close.innerHTML = "imagenC"
+ 			imagenC = document.createElement("img");
+ 			imagenC.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+ 			close.appendChild(imagenC);
+			modal.appendChild(close); 		
+ 			work.appendChild(modal);
+ 			//bodyModal.appendChild(close);
+ 			
+ 			close.addEventListener("click", function(){
+ 				modal.classList.add("hide");
+ 				work.removeChild(modal);
+ 			});
  		//alert(box.innerHTML);
- 		});
+ 	});
  	});
 
  })();
