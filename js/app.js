7315a446c81a671eef8d0ce5f31d2ea531795774
PauLeeR <lindaliel@gmail.com>
2017-06-01 23:42:58 -0400

    cambios en js, ya no se replican todas las fotos pero sigue sin funcionar el modal =(

--- js/app.js
+++ js/app.js
@@ -3,32 +3,41 @@
  */
  (function main(){
  	var boxes = Array.from(document.getElementsByClassName("col-4"));
- 	var work = document.getElementById("work");
+ 	var work = document.getElementById("modal-work");
  	
- 	var modal, bodyModal, close, imagenC;
+ 	var modal, close, imagenC;
  	boxes.forEach(function(box){
  		box.addEventListener("click", function(){
- 			//modal.innerHTML = "";
+ 			work.innerHTML = ""; //esto es para dejar en blanco todo lo que está dentro del modal(es) work
  			modal = document.createElement("div");
  			modal.classList.add("modal-work");
- 			bodyModal = document.createElement("div");
+ 			modal.innerHTML = box.innerHTML;
+
+ 			work.appendChild(modal);
+ 			work.classList.remove("hide");
+ 			
+ 			/*bodyModal = document.createElement("div");
  			bodyModal.classList.add("modal-body");
  			bodyModal.innerHTML = box.innerHTML;
- 			modal.appendChild(bodyModal);  
- 			//creando eleementos de manera dinàmica
+ 			modal.appendChild(bodyModal);*/
+
+ 			//creando elementos de manera dinàmica, aquí el ícono X de cerrar irá en otro div
  			close = document.createElement("div");
  			close.classList.add("close");
  			//close.innerHTML = "imagenC"
+
+ 			//creo el elemento imagen con su src 
  			imagenC = document.createElement("img");
  			imagenC.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
- 			close.appendChild(imagenC);
-			modal.appendChild(close); 		
- 			work.appendChild(modal);
- 			//bodyModal.appendChild(close);
  			
+ 			close.appendChild(imagenC); //el icono X se "apenda" a close y esto al modal
+			work.appendChild(close); 		
+ 			
+ 			//work.appendChild(modal);
+ 			//bodyModal.appendChild(close);
  			close.addEventListener("click", function(){
- 				modal.classList.add("hide");
- 				work.removeChild(modal);
+ 				work.classList.add("hide");
+ 				//work.removeChild(modal);
  			});
  		//alert(box.innerHTML);
  	});
